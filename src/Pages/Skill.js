46ce3a731b90b1dba9d3a93a891e39a875e20274
AvnishT24 <avnish.t@rejolut.com>
2024-02-13 19:45:28 +0530

    left with hamburger

--- src/Pages/Skill.js
+++ src/Pages/Skill.js
@@ -22,17 +22,17 @@ const Skill = () => {
     }
 ]
   return (
-    <div className='px-5 md:px-20 ' id="Skills">
+    <div className='px-2 md:px-20 ' id="Skills">
 <h1 className='text-4xl font-semibold '> Ski<span className='under'>ll's</span></h1>
 <div className='my-10 flex justify-evenly '>
     {skill.map((element , index)=>{
         return<div className='flex flex-col  items-center' key={index}>
         <div className='p-2 border-2 rounded-full'>
-        <img src={element.img} className='w-14 h-14  object-contain' alt="" /> 
+        <img src={element.img} className='w-12 h-12 sm:w-14 sm:h-14  object-contain' alt="" /> 
         
     </div>
     <div className='mt-5 '>
-        <p className='text-xl font-medium'>{element.name}</p>
+        <p className='text-xl font-medium hidden md:block'>{element.name}</p>
     </div>
     </div>
     })}
